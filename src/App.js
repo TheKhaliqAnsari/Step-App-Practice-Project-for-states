57f475773c5e55a5dfd2ab9432b820c432542eed
TheKhaliqAnsari <khaliqansari771@gmail.com>
2023-11-12 03:01:50 +0530

    Created a simple react project for steps to acheive goals for states practice in react.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+    "My password is khaliq@123"
+  ];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsopen] = useState(true);
+  // const step = 1;
+
+  const handleButton = (e) => {
+    if(e.target.value === "next" && step <3){
+      console.log("inside", step)
+      setStep((prev) => prev + 1)
+    } if (e.target.value === "prev" && step > 1) {
+      setStep((prev) => prev - 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsopen(!isOpen)}>&times;</button>
+      {isOpen ? (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step > 0 ? "active" : ""}>1</div>
+            <div className={step > 1 ? "active" : ""}>2</div>
+            <div className={step > 2 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              value="prev"
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={(e) => handleButton(e)}
+            >
+              Prev
+            </button>
+            <button
+              value="next"
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={(e) => handleButton(e)}
+            >
+              {" "}
+              Next{" "}
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 }
-
-export default App;
